50eef7b1f385969d456c65161fed6759ce67e78f
NhieuThinh <82274847+NhieuThinh@users.noreply.github.com>
2024-04-29 13:43:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import pandas as pd
 from flask import Flask, jsonify
 import pickle
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app) 
 
 # Load the saved model
 # with open("C:\\Users\\INTEL\\Downloads\\RecommendSys\\trained\\als_model.pkl", "rb") as f:
